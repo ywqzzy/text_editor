b37ad88626fbfdab97e2aaf00244fe97c109ec38
ywqzzy <592838129@qq.com>
2019-05-25 12:14:27 +0800

    add key config, add more command

--- kilo.c
+++ kilo.c
@@ -15,16 +15,29 @@ enum editorKey {
     ARROW_LEFT = 1000,
     ARROW_RIGHT, 
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 char testC;
 
 /*** data ***/
+typedef struct erow {
+    int size;
+    char *chars;
+} erow;
+
+
 struct editorConfig {
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow row;
     struct termios orig_termios;
 };
 struct editorConfig E;
@@ -78,18 +91,41 @@ int editorReadKey() {
       if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
       if(seq[0] == '[') {
+
+        if (seq[1] >= '0' && seq[1] <= '9') {
+          if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+          if (seq[2] == '~') {
+            switch (seq[1]) {
+              case '1': return HOME_KEY;
+              case '3': return DEL_KEY;
+              case '4': return END_KEY;
+              case '5': return PAGE_UP;
+              case '6': return PAGE_DOWN;
+              case '7': return HOME_KEY;
+              case '8': return END_KEY;
+            }
+          }
+        } else {
           switch (seq[1])
           {
           case 'A': return ARROW_UP;
           case 'B': return ARROW_DOWN;
           case 'C': return ARROW_RIGHT;
           case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
           }
-      } 
+        }
+      }
+      else if(seq[0] == 'O') {
+        switch (seq[1]) {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
+        }
+      }
       return '\x1b';
-    }
-    else {
-        return c;
+    } else {
+      return c;
     }
 }
 
@@ -228,6 +264,21 @@ void editorProcessKeyPress() {
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+      case HOME_KEY:
+        E.cx = 0;
+        break;
+      case END_KEY:
+        E.cx = E.screencols - 1;
+        break;
+      case PAGE_UP:
+      case PAGE_DOWN: 
+       {
+          int times = E.screenrows;
+          while(times --) {
+              editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+          }
+       }
+       break;
       case ARROW_UP:
       case ARROW_DOWN:
       case ARROW_LEFT:
@@ -242,6 +293,7 @@ void editorProcessKeyPress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) {
         die("getWindowSize");
     }
@@ -259,5 +311,3 @@ int main() {
     printf("shit\n");
     return 0;
 }
-
-
